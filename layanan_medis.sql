bec2dd29fcba83c23b984e37ede343c7be7f81ab
indoc4728-hub <indoc4728@gmail.com>
2026-06-07 10:10:11 +0700

    tambah database layanan medis

---
+++ layanan_medis.sql
@@ -0,0 +1,164 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jun 07, 2026 at 02:50 AM
+-- Server version: 8.0.30
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `layanan_medis`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pasien`
+--
+
+CREATE TABLE `pasien` (
+  `id_pasien` int NOT NULL,
+  `nama` varchar(100) DEFAULT NULL,
+  `usia` int DEFAULT NULL,
+  `jenis_pasien` enum('BPJS','ASURANSI','UMUM') DEFAULT NULL,
+  `lama_rawat` int DEFAULT NULL,
+  `biaya_kamar_per_hari` decimal(15,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pasien_asuransi`
+--
+
+CREATE TABLE `pasien_asuransi` (
+  `id_asuransi` int NOT NULL,
+  `id_pasien` int DEFAULT NULL,
+  `nama_provider` varchar(100) DEFAULT NULL,
+  `nomor_polis` varchar(50) DEFAULT NULL,
+  `limit_cover` decimal(15,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pasien_bpjs`
+--
+
+CREATE TABLE `pasien_bpjs` (
+  `id_bpjs` int NOT NULL,
+  `id_pasien` int DEFAULT NULL,
+  `nomor_pbi` varchar(50) DEFAULT NULL,
+  `faskes_asal` varchar(100) DEFAULT NULL,
+  `kelas_kamar` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pasien_umum`
+--
+
+CREATE TABLE `pasien_umum` (
+  `id_umum` int NOT NULL,
+  `id_pasien` int DEFAULT NULL,
+  `nik` varchar(20) DEFAULT NULL,
+  `metode_pembayaran` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `pasien`
+--
+ALTER TABLE `pasien`
+  ADD PRIMARY KEY (`id_pasien`);
+
+--
+-- Indexes for table `pasien_asuransi`
+--
+ALTER TABLE `pasien_asuransi`
+  ADD PRIMARY KEY (`id_asuransi`),
+  ADD KEY `id_pasien` (`id_pasien`);
+
+--
+-- Indexes for table `pasien_bpjs`
+--
+ALTER TABLE `pasien_bpjs`
+  ADD PRIMARY KEY (`id_bpjs`),
+  ADD KEY `id_pasien` (`id_pasien`);
+
+--
+-- Indexes for table `pasien_umum`
+--
+ALTER TABLE `pasien_umum`
+  ADD PRIMARY KEY (`id_umum`),
+  ADD KEY `id_pasien` (`id_pasien`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `pasien`
+--
+ALTER TABLE `pasien`
+  MODIFY `id_pasien` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pasien_asuransi`
+--
+ALTER TABLE `pasien_asuransi`
+  MODIFY `id_asuransi` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pasien_bpjs`
+--
+ALTER TABLE `pasien_bpjs`
+  MODIFY `id_bpjs` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pasien_umum`
+--
+ALTER TABLE `pasien_umum`
+  MODIFY `id_umum` int NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `pasien_asuransi`
+--
+ALTER TABLE `pasien_asuransi`
+  ADD CONSTRAINT `pasien_asuransi_ibfk_1` FOREIGN KEY (`id_pasien`) REFERENCES `pasien` (`id_pasien`);
+
+--
+-- Constraints for table `pasien_bpjs`
+--
+ALTER TABLE `pasien_bpjs`
+  ADD CONSTRAINT `pasien_bpjs_ibfk_1` FOREIGN KEY (`id_pasien`) REFERENCES `pasien` (`id_pasien`);
+
+--
+-- Constraints for table `pasien_umum`
+--
+ALTER TABLE `pasien_umum`
+  ADD CONSTRAINT `pasien_umum_ibfk_1` FOREIGN KEY (`id_pasien`) REFERENCES `pasien` (`id_pasien`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
