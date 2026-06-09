466f1cb0d9cf1f8f2cdfd832271c16ad151d2e8d
indoc4728-hub <indoc4728@gmail.com>
2026-06-09 21:24:10 +0700

    add path model DIR

--- models/pasienasuransiswasta.php
+++ models/pasienasuransiswasta.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'Pasien.php';
+require_once __DIR__ . 'Pasien.php';
 
 class PasienAsuransiSwasta extends Pasien {
     private $namaProvider;
--- models/pasienumum.php
+++ models/pasienumum.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'Pasien.php';
+require_once __DIR__ . 'Pasien.php';
 
 class PasienUmum extends Pasien {
     private $nik;
